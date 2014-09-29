cda1bc9443078245def697ae94f40481960e7dd1
Anurag Sharma <anssharma61@gmail.com>
2014-09-29 21:57:24 +0530

    kickstart

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ earl
 ====
 
 A python script that can format(shorten it/ expand it) a given URL.
+
+Usage
+====
+
+    python earl.py <function> <url>
+    
+    fucntions: make_short, make_long
+    url: The url to operate on.
---
+++ earl.py
@@ -0,0 +1,61 @@
+import urllib2
+import json
+import sys
+
+help_text = '''Usage
+-----
+python earl.py <function> <url>
+-----
+fucntions: make_short, make_long
+url: The url to operate on.
+'''
+opener = urllib2.build_opener()
+
+
+def make_short(url):
+    request = urllib2.Request('https://www.googleapis.com/urlshortener/v1/url',
+                              data=json.dumps({"longUrl": url}),
+                              headers={'Content-Type': 'application/json'})
+    try:
+        response = opener.open(request)
+    except Exception, e:
+        print "Error occurred while making the request."
+        print e
+        sys.exit()
+    data = (response.read()).split(',')[1]
+    short_url = data.split()[1]
+    return short_url
+
+
+def make_long(url):
+    request = urllib2.Request('https://www.googleapis.com/urlshortener\
+        /v1/url?shortUrl=%s' % (url))
+    try:
+        response = opener.open(request)
+    except Exception, e:
+        print "Error occurred while making the request."
+        print e
+        sys.exit()
+
+    data = (response.read()).split(',')[2]
+    long_url = data.split()[1]
+    return long_url
+
+if len(sys.argv) == 1:
+    print help_text
+    sys.exit()
+elif len(sys.argv) == 3:
+    args = list(sys.argv)
+    args.remove(args[0])
+    func = args[0]
+    url = args[1]
+    if func == 'make_long':
+        result = make_long(url)
+        print result
+    elif func == 'make_short':
+        result = make_short(url)
+        print result
+    else:
+        print help_text
+else:
+    print help_text
